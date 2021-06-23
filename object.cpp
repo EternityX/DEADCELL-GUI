de5a065c1dcec8f349efa84b55c364eccd6e258d
eternity <jeramie@protonmail.com>
2021-06-23 13:46:58 -0700

    begin object class

--- object.cpp
+++ object.cpp
@@ -1,2 +1,43 @@
 #include "control.h"
+#include "object.h"
 
+#include <utility>
+
+namespace deadcell::gui {
+    object::object(std::shared_ptr<object> parent)
+        : parent_(std::move(parent)) {
+        if (parent_) {
+            parent_->add_child(shared_from_this());
+        }
+    }
+
+    object::~object() {
+        auto children = std::move(children_);
+
+        for (auto &child : children) {
+            child->parent_ = nullptr;
+        }
+
+        if (parent_) {
+            parent_->remove_child(shared_from_this());
+        }
+    }
+
+    void object::add_child(const std::shared_ptr<object> &object) {
+        object->parent_ = shared_from_this();
+        children_.emplace(children_.begin(), object);
+    }
+
+    void object::remove_child(const std::shared_ptr<object> &object) {
+        for (size_t i = 0; i < children_.size(); ++i) {
+
+            auto &element = children_.at(i);
+            if (element != object) {
+                continue;
+            }
+
+            element->parent_ = nullptr;
+            children_.erase(children_.begin() + i);
+        }
+    }
+}
--- object.h
+++ object.h
@@ -4,11 +4,21 @@
 
 #include <memory>
 #include <string>
+#include <vector>
 
 namespace deadcell::gui {
     class object : public std::enable_shared_from_this<object> {
     private:
+        std::shared_ptr<object> parent_ = nullptr;
+        std::vector<std::shared_ptr<object>> children_;
+
+    protected:
+        object(std::shared_ptr<object> parent = nullptr);
+
     public:
-        
+        virtual ~object();
+
+        void add_child(const std::shared_ptr<object> &object);
+        void remove_child(const std::shared_ptr<object> &object);
     };
 }
