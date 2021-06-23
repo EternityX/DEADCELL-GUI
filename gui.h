208e667ee1353de8ed531377712a158cc032f5f5
eternity <jeramie@protonmail.com>
2021-06-23 12:54:34 -0700

    setup project skeleton

--- gui.h
+++ gui.h
@@ -2,10 +2,7 @@
 
 #define IMGUI_DEFINE_MATH_OPERATORS
 #include <imgui.h>
-
 #include <imgui_internal.h>
-
-#include "layout.h"
 
 #include <d3d9.h>
 
@@ -14,13 +11,4 @@ namespace deadcell::gui {
     inline ImDrawList *bg_draw_list;
 
     inline IDirect3DDevice9 *d3d_device;
-
-    namespace colors {
-        inline auto titlebar = IM_COL32(30, 31, 41, 255);
-        inline auto sidebar = IM_COL32(30, 31, 41, 255);
-        inline auto border = IM_COL32(23, 24, 31, 255);
-        inline auto content = IM_COL32(40, 42, 54, 255);
-
-        inline auto shadow = IM_COL32(0, 0, 0, 255);
-    }
 }
---
+++ object.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "gui.h"
+
+#include <memory>
+#include <string>
+
+namespace deadcell::gui {
+    class object : public std::enable_shared_from_this<object> {
+    private:
+    public:
+        
+    };
+}
