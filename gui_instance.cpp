c925448440485523698daaabef5e056eb65e84aa
eternity <eternity@enevade.org>
2021-06-29 01:52:32 -0700

    Forgot to push gui_instance.cpp.

---
+++ gui_instance.cpp
@@ -0,0 +1,20 @@
+#include "gui_instance.h"
+
+deadcell::gui::gui_instance::gui_instance() {
+    window_manager_ = std::make_unique<window_manager>();
+
+    fonts::titlebar_font = platform::create_font_from_data(cerebri_sans_medium_ttf, sizeof(cerebri_sans_medium_ttf), 24.0f);
+
+    set_dpi_scale(1.0f);
+
+    initialized_ = true;
+}
+
+deadcell::gui::gui_instance::~gui_instance() {
+
+}
+
+void deadcell::gui::gui_instance::set_dpi_scale(const float scale) {
+    ImGuiIO &io = ImGui::GetIO();
+    io.FontGlobalScale = scale;
+}
