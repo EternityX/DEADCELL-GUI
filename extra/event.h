EternityX/DEADCELL-GUI

commit 4fc789713708c9096c2d81c2f89420fd64a31240

- Add base `mouse_move` event. - Remove unused `window_event` events.

extra/event.h
@@ -12,7 +12,8 @@ namespace deadcell::gui {
             mouse_click,
             mouse_down,
             mouse_up,
-            hover,
+            mouse_move,
+            mouse_hover,
         };
 
         base_event() { }
@@ -28,7 +29,7 @@ namespace deadcell::gui {
             return accepted_;
         }
 
-        // Prevents events from being passed down to any children.
+        // Prevent events from being passed down to any children.
         void accept() {
             accepted_ = true;
         }
@@ -43,11 +44,6 @@ namespace deadcell::gui {
     private:
     public:
         enum type_e {
-            mouse_move,
-            left_mouse_down,
-            left_mouse_up,
-            right_mouse_down,
-            right_mouse_up,
             drag_start,
             drag_end,
             resize_start,

controls/button.cpp
@@ -20,7 +20,7 @@ namespace deadcell::gui {
     }
 
     void button::event(base_event &e) {
-        const bool valid = hovered_ && visible_ && enabled_;
+        const bool valid = enabled_ && visible_ && hovered_;
 
         if (e.type() == base_event::mouse_click) {
             if (valid) {
@@ -31,12 +31,12 @@ namespace deadcell::gui {
 
         if (e.type() == base_event::mouse_up) {
             if (valid && mouse_clicked_) {
-                func_();
                 mouse_clicked_ = false;
+                func_();
             }
         }
 
-        if (e.type() == base_event::hover) {
+        if (e.type() == base_event::mouse_hover) {
             if (visible_ && enabled_) {
                 hovered_ = input::is_mouse_in_bounds(pos_, size_);
             }

controls/window.cpp
@@ -61,9 +61,9 @@ namespace deadcell::gui {
 			}
 		}
 
-		// pass base events down to any potential children.
+		// Pass base events down to any potential children.
 		if (e.type() == base_event::mouse_click || e.type() == base_event::mouse_down || e.type() == base_event::mouse_up
-			|| e.type() == base_event::hover) {
+			|| e.type() == base_event::mouse_move || e.type() == base_event::mouse_hover) {
 			e.ignore();
 		}
 

extra/window_manager.cpp
@@ -48,8 +48,6 @@ namespace deadcell::gui {
     }
 
     std::shared_ptr<window> window_manager::get_window_under_cursor() {
-        const auto &io = ImGui::GetIO();
-
         std::deque<window_ptr> windows;
 
         for (auto &win : windows_) {
@@ -84,8 +82,11 @@ namespace deadcell::gui {
         static bool is_dragging = false, is_resizing = false;
         static bool titlebar_hovered = false, resize_hovered = false;
 
+        const point current_mouse_pos = input::get_mouse_pos();
+        static point last_mouse_pos = current_mouse_pos;
+
         if (hovered_window) {
-            hovered_window->dispatch_event(base_event::hover);
+            hovered_window->dispatch_event(base_event::mouse_hover);
 
             const auto bottom_right = hovered_window->get_position() + hovered_window->get_size();
 
@@ -149,6 +150,14 @@ namespace deadcell::gui {
                 }
             }
         }
+
+        if (last_mouse_pos != current_mouse_pos) {
+            if (hovered_window) {
+                hovered_window->dispatch_event(base_event::mouse_move);
+            }
+        }
+
+        last_mouse_pos = current_mouse_pos;
     }
 
     void window_manager::render() {

commit cd34249236c3b6c58127c963108a721ce88657ce

Fix bug introduced in 4fc789713708c9096c2d81c2f89420fd64a31240

extra/event.h
@@ -14,6 +14,7 @@ namespace deadcell::gui {
             mouse_up,
             mouse_move,
             mouse_hover,
+            max_base_event
         };
 
         base_event() { }
@@ -44,7 +45,7 @@ namespace deadcell::gui {
     private:
     public:
         enum type_e {
-            drag_start,
+            drag_start = max_base_event + 1,
             drag_end,
             resize_start,
             resize_end,